f22d61ed29774f992ee9667b761bdcd0340f5b43
Philippe Ballandras <pballandras@coveo.com>
2022-01-25 10:28:11 -0500

    refactor on logging functions

--- logging/logging.go
+++ logging/logging.go
@@ -1,70 +1,80 @@
 package logging
 
 import (
+	"fmt"
 	"log"
-	"strings"
 )
 
 const (
-	errorPrefix   string = "[ERROR]"
-	warningPrefix string = "[WARN]"
-	infoPrefix    string = "[INFO]"
-	debugPrefix   string = "[DEBUG]"
-	tracePrefix   string = "[TRACE]"
+	ErrorLevel   string = "ERROR"
+	WarningLevel string = "WARN"
+	InfoLevel    string = "INFO"
+	DebugLevel   string = "DEBUG"
+	TraceLevel   string = "TRACE"
 )
 
 func init() {
 	log.SetFlags(0) // Removes all logger prefixes
 }
 
+func formatLogLevel(logLevel string) string {
+	return fmt.Sprintf("[%s]", logLevel)
+}
+
 func prefixLogArgs(prefix string, args ...interface{}) []interface{} {
 	return append([]interface{}{prefix}, args...)
 }
 
-func prefixLogFormat(prefix, format string) string {
-	var builder strings.Builder
-	builder.WriteString(prefix)
-	builder.WriteString(" ")
-	builder.WriteString(format)
-	return builder.String()
-}
-
 func Error(args ...interface{}) {
-	log.Println(prefixLogArgs(errorPrefix, args...)...)
+	log.Println(prefixLogArgs(formatLogLevel(ErrorLevel), args...)...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Printf(prefixLogFormat(errorPrefix, format), args...)
+	log.Printf("[%s] "+format, prefixLogArgs(ErrorLevel, args...)...)
 }
 
 func Warning(args ...interface{}) {
-	log.Println(prefixLogArgs(warningPrefix, args...)...)
+	log.Println(prefixLogArgs(formatLogLevel(WarningLevel), args...)...)
 }
 
 func Warningf(format string, args ...interface{}) {
-	log.Printf(prefixLogFormat(warningPrefix, format), args...)
+	log.Printf("[%s] "+format, prefixLogArgs(WarningLevel, args...)...)
 }
 
 func Info(args ...interface{}) {
-	log.Println(prefixLogArgs(infoPrefix, args...)...)
+	log.Println(prefixLogArgs(formatLogLevel(InfoLevel), args...)...)
 }
 
 func Infof(format string, args ...interface{}) {
-	log.Printf(prefixLogFormat(infoPrefix, format), args...)
+	log.Printf("[%s] "+format, prefixLogArgs(InfoLevel, args...)...)
 }
 
 func Debug(args ...interface{}) {
-	log.Println(prefixLogArgs(debugPrefix, args...)...)
+	log.Println(prefixLogArgs(formatLogLevel(DebugLevel), args...)...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	log.Printf(prefixLogFormat(debugPrefix, format), args...)
+	log.Printf("[%s] "+format, prefixLogArgs(DebugLevel, args...)...)
 }
 
 func Trace(args ...interface{}) {
-	log.Println(prefixLogArgs(tracePrefix, args...)...)
+	log.Println(prefixLogArgs(formatLogLevel(TraceLevel), args...)...)
 }
 
 func Tracef(format string, args ...interface{}) {
-	log.Printf(prefixLogFormat(tracePrefix, format), args...)
+	log.Printf("[%s] "+format, prefixLogArgs(TraceLevel, args...)...)
+}
+
+func getLoggingFuncByLevel(level string) func(format string, args ...interface{}) {
+	fun, found := map[string]func(format string, args ...interface{}){
+		ErrorLevel:   Errorf,
+		WarningLevel: Warningf,
+		InfoLevel:    Infof,
+		DebugLevel:   Debugf,
+		TraceLevel:   Tracef,
+	}[level]
+	if !found {
+		fun = Tracef // But really, please don't get here?
+	}
+	return fun
 }
