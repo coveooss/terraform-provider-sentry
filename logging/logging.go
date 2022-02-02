1feefd0ce848024f05e2665aaa784bd14c0f2bef
Philippe Ballandras <pballandras@coveo.com>
2022-02-02 15:32:06 -0500

    removed custom logging code

--- logging/logging.go
+++ logging/logging.go
@@ -1,82 +1,26 @@
 package logging
 
-import (
-	"fmt"
-	"log"
-)
-
-type LogLevel int
-
-func (level LogLevel) String() string {
-	return []string{"ERROR", "WARN", "INFO", "DEBUG", "TRACE"}[level]
+// LogKeyValuePair is a helper struct to help organise the use of tflog. tflog
+// uses pairs to print key-value items/args, so this just to help visualize the
+// way the logging works
+type LogKeyValuePair struct {
+	// Key is what will appear next to the value, use it as a small hint/description
+	Key interface{}
+	// The value is what you want to log.
+	Value interface{}
 }
 
-const (
-	ErrorLevel LogLevel = iota
-	WarningLevel
-	InfoLevel
-	DebugLevel
-	TraceLevel
-)
-
-func init() {
-	log.SetFlags(0) // Removes all logger prefixes
+func MakePair(key, value interface{}) LogKeyValuePair {
+	return LogKeyValuePair{
+		Key:   key,
+		Value: value,
+	}
 }
 
-func formatLogLevel(logLevel LogLevel) string {
-	return fmt.Sprintf("[%s]", logLevel)
-}
-
-func prefixLogArgs(logLevel string, args ...interface{}) []interface{} {
-	return append([]interface{}{logLevel}, args...)
-}
-
-func Error(args ...interface{}) {
-	log.Println(prefixLogArgs(formatLogLevel(ErrorLevel), args...)...)
-}
-
-func Errorf(format string, args ...interface{}) {
-	log.Printf("[%s] "+format, prefixLogArgs(ErrorLevel.String(), args...)...)
-}
-
-func Warning(args ...interface{}) {
-	log.Println(prefixLogArgs(formatLogLevel(WarningLevel), args...)...)
-}
-
-func Warningf(format string, args ...interface{}) {
-	log.Printf("[%s] "+format, prefixLogArgs(WarningLevel.String(), args...)...)
-}
-
-func Info(args ...interface{}) {
-	log.Println(prefixLogArgs(formatLogLevel(InfoLevel), args...)...)
-}
-
-func Infof(format string, args ...interface{}) {
-	log.Printf("[%s] "+format, prefixLogArgs(InfoLevel.String(), args...)...)
-}
-
-func Debug(args ...interface{}) {
-	log.Println(prefixLogArgs(formatLogLevel(DebugLevel), args...)...)
-}
-
-func Debugf(format string, args ...interface{}) {
-	log.Printf("[%s] "+format, prefixLogArgs(DebugLevel.String(), args...)...)
-}
-
-func Trace(args ...interface{}) {
-	log.Println(prefixLogArgs(formatLogLevel(TraceLevel), args...)...)
-}
-
-func Tracef(format string, args ...interface{}) {
-	log.Printf("[%s] "+format, prefixLogArgs(TraceLevel.String(), args...)...)
-}
-
-func getLoggingFuncByLevel(level LogLevel) func(format string, args ...interface{}) {
-	return []func(format string, args ...interface{}){
-		Errorf,
-		Warningf,
-		Infof,
-		Debugf,
-		Tracef,
-	}[level]
+func UnpackPairs(pairs ...LogKeyValuePair) (args []interface{}) {
+	args = make([]interface{}, 2*len(pairs))
+	for _, pair := range pairs {
+		args = append(args, pair.Key, pair.Value)
+	}
+	return args
 }
